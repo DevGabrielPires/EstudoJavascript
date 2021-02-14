4652d776f859b28bf460c7e0819d844e3ac859db
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-02-14 11:01:24 -0300

    finalizando aula 1

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+console.log('meu primeiro programa');
+
+// guardando valores dentro da variaveis
+
+const idade = 24;
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade + 4;
+console.log(idadeSomada);
+
+
