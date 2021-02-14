4b59a90dd90eacf9ff971125502809d2748d24bd
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-02-14 11:11:12 -0300

    finalizando aula 2

---
+++ trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+// Console.log('trabalhando com variáveis');
+
+console.log('trabalhando com variáveis');
+// JS é case sensitive
+
+const idade = 24;
+const nome = "Gabriel";
+
+// não fazer
+// mes = "janeiro"
