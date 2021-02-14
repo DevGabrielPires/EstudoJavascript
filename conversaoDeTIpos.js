1ef5b5aca2b1139b37b4eb0133381975274258f4
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-02-14 11:44:29 -0300

    finalizando exercicio 4

---
+++ conversaoDeTIpos.js
@@ -0,0 +1,12 @@
+console.log('conversão de tipos');
+
+
+console.log("ano" + 2021);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("7" / "2");
+console.log("ricardo" / "2"); //not a number
+
+console.log(6.5);
+console.log(6,5);
