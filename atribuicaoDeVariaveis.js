eccdc17ffc0e65c0c993864041301008a1f766de
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-02-14 12:11:40 -0300

    terminando exercicio 5

---
+++ atribuicaoDeVariaveis.js
@@ -0,0 +1,16 @@
+console.log('Trabalhando com atribuições de variáveis');
+
+const idade = 24;
+const nome = "gabriel";
+const sobrenome = "pires";
+
+//console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`menu nome é ${nome} ${sobrenome}`) //template string  
+
+let contador = 0;
+contador += 1;
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
