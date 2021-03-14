e424379db424b4c5f96dab4b49bfeb5c7e6d2cdf
Gabriel Pires <gabriel.r.pires@outlook.com>
2021-03-14 07:59:13 -0300

    criando pasta do curso

--- OrientacaoObjeto/index.js
+++ OrientacaoObjeto/index.js
@@ -1,14 +1,11 @@
 import { Cliente } from './Cliente.js';
 import { ContaCorrente } from './ContaCorrente.js';
 
-
 const cliente1 = new Cliente();
-
 cliente1.nome = "Gabriel";
 cliente1.cpf = 5558880896;
 
 const cliente2 = new Cliente();
-
 cliente2.nome = "Fran";
 cliente2.cpf = 54458745698;
 
@@ -22,9 +19,7 @@ contaCorrenteGabriel.depositar(10);
 
 const valorSacado = contaCorrenteGabriel.sacar(400);
 
-
 console.log(valorSacado);
-
 
 // console.log(cliente1);
 // console.log(cliente2);
