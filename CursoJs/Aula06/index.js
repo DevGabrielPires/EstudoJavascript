2ea9c8ff6207d6c72074e036fd311985dc619342
Gabriel Pires <gabriel.r.pires@outlook.com>
2021-03-16 22:07:05 -0300

    finalizando aula 6

---
+++ CursoJs/Aula06/index.js
@@ -0,0 +1,4 @@
+const nome = "joão";
+
+console.log(`${nome} nasceu em 1984.`);
+console.log(`Em 2000 ${nome} conheceu Maria`);
---
+++ CursoJs/aula05/mains.js
@@ -0,0 +1,4 @@
+console.log("este trecho será exibido no console do navegador");
+console.log("Este também");
+window.alert("Mensagem irritante");
+window.alert("Outra mensagem irritante");
